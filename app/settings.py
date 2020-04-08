4d9b8be4ee722066da0006df97ccf1d6c5b59449
Adela Skotak <skotak.adela@gmail.com>
2020-04-08 18:25:07 +0200

    Added redirect

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE3') == 'True')
-
+# DEBUG = True
 ALLOWED_HOSTS = ['0.0.0.0', 'asko-recipeapp.herokuapp.com']
 
 
